bb214bfdf6f5f9c36c753d01ce4ae1698712a7d3
Angel <santoyox714@gmail.com>
2023-02-21 12:57:37 -0800

    add padding under send button

--- src/lib/pages/Content.svelte
+++ src/lib/pages/Content.svelte
@@ -70,7 +70,7 @@
     <input  type="text"class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="message" required bind:value={input} />
     <span class=brsmall></span>
     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded float-right" on:click={add}>send</button>
-
+    <br> <br>
   </Text>
 </Page>
 
