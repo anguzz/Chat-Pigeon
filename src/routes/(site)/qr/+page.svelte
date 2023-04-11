508ab502ccfad467c065a7d4edfc2320f05c6bdd
Angel S <santoyox714@gmail.com>
2023-04-11 14:50:50 -0700

    add qr code

---
+++ src/routes/(site)/qr/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Page from "$lib/components/Page.svelte";
+    import { QRCodeImage } from "svelte-qrcode-image";
+    let chat_url ="https://chatpigeon.vercel.app/chat"
+    import Text from "$lib/components/Text.svelte";
+  </script>
+  
+<Text>
+    <div
+  class="flex flex-col items-center justify-center bg-center bg-no-repeat bg-cover page lg:bg-fixed bg-slate-900"
+  id="bg"
+>
+
+<div class = "size"> 
+  <QRCodeImage text = {chat_url} width=600 height = 600;/>
+</div>
+
+  <div>
+</Text>
+
